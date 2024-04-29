a1998b6be8e0e451d6529aa7a70b134dbb24fec3
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-04-29 18:54:49 -0400

    feat: apiUrl

--- src/services/homeService.ts
+++ src/services/homeService.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const useFetchData = () => {
   return useQuery('data', async () => {
     try {
-      const apiUrl = process.env.NEXT_PUBLIC_APP_API_URL;
+      const apiUrl = "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC"
 
       if (!apiUrl) {
         throw new Error('API URL not defined in environment variables');
@@ -12,6 +12,7 @@ export const useFetchData = () => {
 
       const response = await axios.get(apiUrl);
 
+      
       return response.data.products;
     } catch (error) {
       throw new Error('Failed to fetch data');
